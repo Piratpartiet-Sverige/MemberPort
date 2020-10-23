4f2ed9aa4a087d4e304c8562c13e2ad34b696d19
Marcus Isaksson <me@naknut.email>
2020-10-22 19:15:49 -0700

    Rename property

--- app/web/handlers/admin/edit_organization.py
+++ app/web/handlers/admin/edit_organization.py
@@ -8,7 +8,7 @@ from uuid import UUID
 class EditOrganizationHandler(BaseHandler):
     @tornado.web.authenticated
     async def get(self):
-        organizationId = UUID(self.get_argument("id"))
-        organization = await OrganizationsDao(self.db).get_organization_by_id(organizationId)
+        id = UUID(self.get_argument("id"))
+        organization = await OrganizationsDao(self.db).get_organization_by_id(id)
 
         await self.render("admin/edit-organization.html", admin=True, title="Ändra förening", organization=organization)
