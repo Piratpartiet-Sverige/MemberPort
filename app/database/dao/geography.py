2bf3fed08a699472203fb48c23f0fa42a2bd1768
Jakob Sinclair <sinclair.jakob@mailbox.org>
2020-11-03 16:13:03 +0100

    feat: add dao functions for creating areas (#2)
    Signed-off-by: Jakob Sinclair <sinclair.jakob@mailbox.org>

--- app/database/dao/geography.py
+++ app/database/dao/geography.py
@@ -6,12 +6,57 @@ from asyncpg import Connection
 from asyncpg.exceptions import UniqueViolationError
 
 from app.logger import logger
-from app.models import Country, Municipality
+from app.models import Country, Municipality, Area
 from app.database.dao.base import BaseDao
 
 
 class GeographyDao(BaseDao):
-    async def create_municipality(self, name: str, country_id: Union[UUID, None]) -> bool:
+    async def create_area(self, name: str, country_id: UUID, parent_id: Union[int, None]) -> Area:
+        sql = """
+            WITH i AS (
+                SELECT nextval(pg_get_serial_sequence('areas', 'id')) AS id
+            )
+            INSERT INTO areas (id, name, created, "country", path) SELECT id, $1, $2, $3, CAST($4 || CAST(id AS TEXT) as ltree) FROM i
+            RETURNING id;
+        """
+        created = datetime.utcnow()
+        area_id = 0
+        path = await self._get_path(parent_id)
+
+        try:
+            async with self.pool.acquire() as con:  # type: Connection
+                area_id = await con.fetchval(sql, name, created, country_id, path)
+        except Exception:
+            logger.error("An error occured when trying to create new area!", stack_info=True)
+            return None
+
+        area = Area()
+        area.id = area_id
+        area.country = await self.get_country_by_id(country_id)
+        area.created = created
+
+        return area
+
+    async def _get_path(self, parent_id: Union[int, None]) -> str:
+        if parent_id is None:
+            return ""
+
+        path = ""
+        sql = 'SELECT path FROM areas WHERE id = $1;'
+
+        try:
+            async with self.pool.acquire() as con:  # type: Connection
+                path = await con.fetchval(sql, parent_id)
+        except Exception:
+            logger.error("An error occured when trying to retrieve the path for new area!", stack_info=True)
+            return ""
+
+        path += "."
+        logger.debug("Path for new area: " + path)
+
+        return path
+
+    async def create_municipality(self, name: str, country_id: UUID) -> bool:
         sql = 'INSERT INTO municipalities (id, name, "country", created) VALUES ($1, $2, $3, $4);'
         created = datetime.utcnow()
         municipality_id = uuid4()
@@ -101,6 +146,23 @@ class GeographyDao(BaseDao):
         if row is not None:
             country.id = country_id
             country.name = row["name"]
+            country.created = row["created"]
+        else:
+            return None
+
+        return country
+
+    async def get_country_by_name(self, country_name: str) -> Union[Country, None]:
+        sql = 'SELECT id, created FROM countries WHERE name = $1;'
+
+        async with self.pool.acquire() as con:  # type: Connection
+            row = await con.fetchrow(sql, country_name)
+
+        country = Country()
+
+        if row is not None:
+            country.id = row["id"]
+            country.name = country
             country.created = row["created"]
         else:
             return None
