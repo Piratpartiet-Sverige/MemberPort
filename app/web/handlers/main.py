7631bf7307ffcfa89ad5277a2e651457a2bc2545
Jakob Sinclair <sinclair.jakob@mailbox.org>
2021-02-09 17:51:36 +0100

    feat: add support for RSS feeds
    Signed-off-by: Jakob Sinclair <sinclair.jakob@mailbox.org>

--- app/web/handlers/main.py
+++ app/web/handlers/main.py
@@ -1,5 +1,6 @@
 import tornado.web
 
+from app.database.dao.settings import SettingsDao
 from app.database.dao.users import UsersDao
 from app.logger import logger
 from app.web.handlers.base import BaseHandler
@@ -13,10 +14,14 @@ class MainHandler(BaseHandler):
         dao = UsersDao(self.db)
         permissions_check = await dao.check_user_admin(self.current_user.user.id)
 
+        settings_dao = SettingsDao(self.db)
+        feed_url = await settings_dao.get_feed_url()
+
         await self.render(
             "main.html",
             title="Dashboard",
             admin=permissions_check,
             name=self.current_user.user.name.first + " " + self.current_user.user.name.last,
-            number=self.current_user.user.number
+            number=self.current_user.user.number,
+            feed_url=feed_url
         )
